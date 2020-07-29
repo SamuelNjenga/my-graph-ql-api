f209613d4f396087ee0a3e0c94b73866f94a6837
Samuel Njenga <snjenga704@gmail.com>
2020-07-29 23:44:06 +0300

    Modified server.js by adding mutation functionality for our author and book api

--- server.js
+++ server.js
@@ -99,8 +99,42 @@ resolve:(parent,args) => books.find(book => book.id === args.id)
         }
     })
 })
+const RootMutationType = new GraphQLObjectType({
+    name: 'Mutation',
+    description: 'Root Mutation',
+    fields:() => ({
+        addBook: {
+            type: BookType,
+            description: 'Add a book',
+            args:{
+                name:{type: GraphQLNonNull(GraphQLString)},
+                authorId:{type: GraphQLNonNull(GraphQLInt)}
+            },
+            resolve:(parent,args) => {
+                const book = {id:books.length + 1,name:args.name,authorId:args.authorId}
+                books.push(book)
+                return book;
+            }
+            
+        },
+        addAuthor: {
+            type: AuthorType,
+            description: 'Add an author',
+            args:{
+                name:{type: GraphQLNonNull(GraphQLString)}
+            },
+            resolve:(parent,args) => {
+                const author = {id:authors.length + 1,name:args.name}
+                books.push(author)
+                return author;
+            }
+            
+        }
+    })
+})
 const schema = new GraphQLSchema({
-    query: RootQueryType
+    query: RootQueryType,
+    mutation:RootMutationType
 })
 app.use('/graphql', graphqlHTTP({
     schema: schema,
